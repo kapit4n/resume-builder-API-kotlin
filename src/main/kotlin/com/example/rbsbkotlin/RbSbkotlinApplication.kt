d2b883fad3e2d86b338ccb5d1bec7e89b2f5412d
larce <luis.arce22@gmail.com>
2017-12-29 19:57:32 -0400

    Init resume builder API

---
+++ src/main/kotlin/com/example/rbsbkotlin/RbSbkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rbsbkotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RbSbkotlinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(RbSbkotlinApplication::class.java, *args)
+}
